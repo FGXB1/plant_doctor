4a445dc8954891f76222968c46f23375029dfd91
Ishan Shah <ishanshah175@gmail.com>
2024-06-13 01:47:44 -0400

    created home page foundation, removed about page code since there wasn't anything meaningful, cleaned up code

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,35 +1,95 @@
-import 'dart:ffi';
+// ignore_for_file: must_be_immutable
 
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:plant_doctor/main.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class Buttons extends StatelessWidget {
-  const Buttons({super.key, required this.cardText});
-  final String cardText;
+class Homepage extends StatelessWidget {
+  Homepage({super.key});
+  var hour = DateTime.now().hour;
 
   @override
   Widget build(BuildContext context) {
-    return const Card.filled(
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(5.0))),
-      color: Color(0xFFBEEDD4),
-      child: SizedBox(
-        height: 50,
-        width: 100,
-        child: Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Center(
-            // child: Text(
-            //   cardText,
-            //   style: const TextStyle(fontSize: 12),
-            // )
-            child: Icon(
-              Icons.search,
-              color: Colors.white,
-              semanticLabel: "Explore Plants",
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(hour < 12 ? 'Good Morning' : 'Good Evening'),
+        backgroundColor: kColorScheme.primary,
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: [
+                Text(
+                  'To plant a garden is to believe in tomorrow.',
+                  style: TextStyle(fontSize: 28),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 20,),
+                Text('~ Audrey Hepburn', style: TextStyle(fontSize: 20)),
+              ],
             ),
-          ),
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Buttons(
+                      cardText: 'Explore Plants',
+                      icon: FontAwesomeIcons.magnifyingGlass,
+                    ),
+                    Padding(padding: EdgeInsets.all(8.0)),
+                    Buttons(
+                      cardText: 'Live Connect',
+                      icon: FontAwesomeIcons.whatsapp,
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Buttons(
+                      cardText: 'Common Problems',
+                      icon: FontAwesomeIcons.plantWilt,
+                    ),
+                    Padding(padding: EdgeInsets.all(8.0)),
+                    Buttons(
+                      cardText: 'Calendar',
+                      icon: FontAwesomeIcons.calendar,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class Buttons extends StatelessWidget {
+  const Buttons({super.key, required this.cardText, required this.icon});
+  final String cardText;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: SizedBox(
+        height: 80,
+        child: FilledButton.icon(
+            onPressed: () {},
+            label: Text(cardText),
+            icon: FaIcon(icon),
+            style: FilledButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0)),
+            )),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -6,25 +6,12 @@ void main() {
 }
 
 var kColorScheme = ColorScheme.fromSeed(seedColor: const Color(0xFF6EA71F)).copyWith(
-  onSecondary: Colors.red
+  primary: const Color(0xFF6EA71F)
 );
-
-class StyledText extends StatelessWidget {
-  const StyledText({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Text(
-      'Hello World',
-      style: TextStyle(color: Colors.green, fontSize: 28),
-    );
-  }
-}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -33,63 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: kColorScheme,
         useMaterial3: true,
       ),
-      home: const AboutPage(title: 'About Us'),
-    );
-  }
-}
-
-class AboutPage extends StatelessWidget {
-  const AboutPage({super.key, required this.title});
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: kColorScheme.inversePrimary,
-        title: Text(title),
-        ),
-      body: Center(
-        child: Column(
-          children: [
-            const Center(
-              child: Text(
-                'About us front text', 
-              style: TextStyle(
-                  fontSize: 28
-                ),
-              ),
-            ),
-            Container(
-              height: 50,
-              width: 50,
-              color: kColorScheme.primary,
-              child: Center(
-                child: Text(
-                  "hi", 
-                  style: TextStyle(
-                    color: kColorScheme.onSecondary,
-                  ),
-                )
-              ),
-            ),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              
-              children: [
-                Buttons(cardText: 'Explore Plants'),
-                Buttons(cardText: 'Explore Plants')
-            ],),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Buttons(cardText: 'Explore Plants'),
-                Buttons(cardText: 'Explore Plants')
-            ],),
-            
-          ],
-        ),
-      ),
+      home: Homepage(),
     );
   }
 }
